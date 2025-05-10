57d44bd3dfaa07507c65e60d0d21f612bbaea019
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-05-10 18:00:02 -0300

    Alterado o conteúdo do HERO - Deixado de forma mais minimalista, com a imagem menor e texto em destaque.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,21 @@ import '../styles/Hero.css';
 const Hero = () => {
   return (
     <section className="hero-section fade-in">
-      <div className="hero-overlay">
-        <div className="hero-content">
+      <div className="hero-overlay"></div>
+      <div className="hero-content">
+        <div className="hero-text">
           <h1>Transformando ideias em soluções digitais</h1>
-          <p>Especialistas em criar experiências digitais inovadoras para microempresas</p>
+          <p>Especialistas em criar experiências digitais inovadoras para microempresas.</p>
           <div className="hero-buttons">
             <a href="/contact" className="btn btn-primary">Fale Conosco</a>
             <a href="#about" className="btn btn-secondary">Saiba Mais</a>
           </div>
         </div>
+        <img 
+          className="hero-image" 
+          src="https://i.postimg.cc/3RvsYjwf/Templete.png" 
+          alt="Soluções Digitais" 
+        />
       </div>
     </section>
   );
