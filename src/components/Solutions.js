2f7ce6717f1ce27ff57ec95cd714bbd76a7c7aee
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-05-10 20:01:52 -0300

    Alterado a parto NOSSAS SOLUÇÕES

--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import '../styles/Solutions.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faCode, faTools, faServer } from '@fortawesome/free-solid-svg-icons';
+
+
 const Solutions = () => {
   const services = [
-    'Criação de Sites',
-    'SEO',
-    'Suporte Técnico',
-    'Hospedagem',
+    { name: 'Criação de Sites', icon: faCode},
+    { name: 'SEO', icon: faSearch },
+    { name: 'Suporte Técnico', icon: faTools },
+    { name: 'Hospedagem e Infra', icon: faServer },
   ];
 
   return (
     <section className="solutions-section">
-      <h2>Soluções</h2>
-      <div className="solutions-cards">
-        {services.map((service) => (
-          <div key={service} className="solution-card">
-            <h3>{service}</h3>
-          </div>
+      <h2>Nossas Soluções</h2>
+      <h4>Serviços especializados para ajudar seu negócio digital a decolar! 🚀</h4>
+        <div className="solutions-cards">
+          {services.map((service) => (
+          <div key={service.name} className="solution-card">
+          <FontAwesomeIcon icon={service.icon} size="2x" className="icon" />
+          <h3>{service.name}</h3>
+        </div>
         ))}
-      </div>
+        </div>
     </section>
   );
 };
